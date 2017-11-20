be4c39ea93871f1a08ff8dafc404b6efdb60156e
Shawn Olson <LOCODARWIN@msn.com>
2017-11-20 00:18:56 -0800

    1st draft

--- AlphaPager/Form1.Designer.cs
+++ AlphaPager/Form1.Designer.cs
@@ -28,12 +28,200 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.butLogin = new System.Windows.Forms.Button();
+            this.butConfig = new System.Windows.Forms.Button();
+            this.textCitnum = new System.Windows.Forms.TextBox();
+            this.textPrivvPass = new System.Windows.Forms.TextBox();
+            this.textWorld = new System.Windows.Forms.TextBox();
+            this.textCoords = new System.Windows.Forms.TextBox();
+            this.textAvatar = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBotname = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // butLogin
+            // 
+            this.butLogin.Location = new System.Drawing.Point(36, 106);
+            this.butLogin.Name = "butLogin";
+            this.butLogin.Size = new System.Drawing.Size(128, 35);
+            this.butLogin.TabIndex = 0;
+            this.butLogin.Text = "Login";
+            this.butLogin.UseVisualStyleBackColor = true;
+            // 
+            // butConfig
+            // 
+            this.butConfig.Location = new System.Drawing.Point(204, 106);
+            this.butConfig.Name = "butConfig";
+            this.butConfig.Size = new System.Drawing.Size(137, 35);
+            this.butConfig.TabIndex = 1;
+            this.butConfig.Text = "Configure";
+            this.butConfig.UseVisualStyleBackColor = true;
+            // 
+            // textCitnum
+            // 
+            this.textCitnum.Location = new System.Drawing.Point(64, 39);
+            this.textCitnum.Name = "textCitnum";
+            this.textCitnum.Size = new System.Drawing.Size(100, 20);
+            this.textCitnum.TabIndex = 2;
+            // 
+            // textPrivvPass
+            // 
+            this.textPrivvPass.Location = new System.Drawing.Point(64, 65);
+            this.textPrivvPass.Name = "textPrivvPass";
+            this.textPrivvPass.Size = new System.Drawing.Size(100, 20);
+            this.textPrivvPass.TabIndex = 3;
+            // 
+            // textWorld
+            // 
+            this.textWorld.Location = new System.Drawing.Point(241, 12);
+            this.textWorld.Name = "textWorld";
+            this.textWorld.Size = new System.Drawing.Size(100, 20);
+            this.textWorld.TabIndex = 4;
+            // 
+            // textCoords
+            // 
+            this.textCoords.Location = new System.Drawing.Point(241, 39);
+            this.textCoords.Name = "textCoords";
+            this.textCoords.Size = new System.Drawing.Size(100, 20);
+            this.textCoords.TabIndex = 5;
+            // 
+            // textAvatar
+            // 
+            this.textAvatar.Location = new System.Drawing.Point(241, 65);
+            this.textAvatar.Name = "textAvatar";
+            this.textAvatar.Size = new System.Drawing.Size(44, 20);
+            this.textAvatar.TabIndex = 6;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(6, 153);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(362, 180);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 338);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(374, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Citnum:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Priv Pass:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "World:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(197, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Coords:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(188, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Avatar #:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Botname:";
+            // 
+            // textBotname
+            // 
+            this.textBotname.Location = new System.Drawing.Point(64, 12);
+            this.textBotname.Name = "textBotname";
+            this.textBotname.Size = new System.Drawing.Size(100, 20);
+            this.textBotname.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(374, 360);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBotname);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textAvatar);
+            this.Controls.Add(this.textCoords);
+            this.Controls.Add(this.textWorld);
+            this.Controls.Add(this.textPrivvPass);
+            this.Controls.Add(this.textCitnum);
+            this.Controls.Add(this.butConfig);
+            this.Controls.Add(this.butLogin);
+            this.Name = "Form1";
+            this.Text = "AlphaPager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button butLogin;
+        private System.Windows.Forms.Button butConfig;
+        private System.Windows.Forms.TextBox textCitnum;
+        private System.Windows.Forms.TextBox textPrivvPass;
+        private System.Windows.Forms.TextBox textWorld;
+        private System.Windows.Forms.TextBox textCoords;
+        private System.Windows.Forms.TextBox textAvatar;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBotname;
     }
 }
 
--- AlphaPager/Form1.cs
+++ AlphaPager/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AW;
 
 namespace AlphaPager
 {
@@ -16,5 +17,32 @@ namespace AlphaPager
         {
             InitializeComponent();
         }
+
+
+        public IInstance _instance;
+        public Timer aTimer;
+
+        public static class Globals
+        {
+            public static string sUnivLogin = "auth.activeworlds.com";
+            public static int iPort = 6670;
+            public static string sBotName = "cSharpBot";
+            public static string sBotDesc = "Bot Test";
+            public static int iCitNum = 318855;
+            public static string sPassword = "shamma11";
+            public static string sWorld = "powergod";
+            public static int iXPos = 0;
+            public static int iYPos = 690;
+            public static int iZPos = 500;
+            public static int iYaw = 0;
+            public static int iAV = 1;
+
+            public static bool iInUniv = false;
+            public static bool iInWorld = false;
+        }
+
+
+
+
     }
 }
